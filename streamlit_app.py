79ea95e4fc76fab5f4c2ab75f398a266b974bfda
JLP26 <137760154+JLP26@users.noreply.github.com>
2023-06-26 12:52:09 +0530

    Update streamlit_app.py
    made spacing changes in the existing menu
    added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('My Parents Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Ranged Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
