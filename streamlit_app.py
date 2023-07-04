7b88c8057ce3fd76ff96ee4590f4fc5ee8fd3492
JLP26 <137760154+JLP26@users.noreply.github.com>
2023-07-04 08:30:09 +0530

    Update streamlit_app.py
    replaced fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,14 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list= my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Cantaloupe'])
 fruits_to_show= my_fruit_list.loc[fruits_selected]
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Orange')
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
@@ -41,6 +41,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','cantaloupe')
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
